d9c80844fd8d16b0e795647c3b3c5d0d7c13ed3b
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-04-30 11:21:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const name = 'Labib';
+const age = '20';
+const favouriteLanguage = 'JavaScript';
+
+console.log(`Hey I am ${name}. My age is ${age} right now. Now I am learning ${favouriteLanguage}.`)
