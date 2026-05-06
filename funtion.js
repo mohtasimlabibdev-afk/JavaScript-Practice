afee2d4b15e6d1477530e86f7536003af0a9fddc
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-05-07 01:25:01 +0600

    funtion

---
+++ funtion.js
@@ -0,0 +1,8 @@
+// lol I dont know funtions yet so here is a random one
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Example usage:
+const result = addTwoNumbers(5, 10);
+console.log(result); // Output: 15
