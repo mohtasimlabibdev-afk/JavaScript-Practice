b82e8df6ddb5a07cbd6548ad5b49f94198d985ea
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-05-08 00:52:07 +0600

    funtion multiple paramiter

--- funtion.js
+++ funtion.js
@@ -8,3 +8,25 @@ function turnOn() {
       // Code to turn on the device goes here        
 }
 turnOn();
+
+// Function Parameter, Handle Multiple Parameters
+
+function square (lol){
+      console.log(lol);
+      const result = lol * lol;
+      console.log(`The square of ${lol} is: ${result}`);
+}
+square(5);
+square(10);
+square(12);
+
+// Add two numbers
+function add(num1, num2){
+
+      const sum = num1 + num2;
+      console.log(`The sum of ${num1} and ${num2} is:`);
+      console.log(sum);
+}
+add(4, 6);
+add(14, 6);
+add(24, 6);
