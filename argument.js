66f16bf2f9d32db7c24e20304c1f3c9707ce12ad
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-05-08 01:07:32 +0600

    multiple arguments

---
+++ argument.js
@@ -0,0 +1,19 @@
+// Double It
+function doubleIt(number){
+      const doubled = number * 2;
+      console.log(number, doubled);
+}
+doubleIt(5);
+console.log("-------------");
+doubleIt(10);
+console.log("-------------");
+doubleIt(15);
+
+// difference 
+function difference(num1, num2){
+      const diff = num1 - num2;
+      console.log('father age is', num1, 'son age is', num2, 'difference is', diff);
+}
+const fatherAge = 50;
+const sonAge = 20;
+difference(fatherAge, sonAge);
