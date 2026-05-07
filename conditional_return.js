7acea32ab8d66363cef9df9329ba8b319ccd3ad3
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-05-08 02:23:28 +0600

    conditional return

---
+++ conditional_return.js
@@ -0,0 +1,20 @@
+function isEven(number) {
+      if (number % 2 === 0) {
+            return true;
+      } else {
+            return false;
+      }     
+}
+// console.log(isEven(5));
+// console.log(isEven(10));
+
+function ifOdd(number) {
+      if (number % 2 === 1) {
+            return true;
+      } else {
+            return false;
+      }     
+}
+
+console.log(ifOdd(5));
+console.log(ifOdd(10));
---
+++ recap.js
@@ -0,0 +1,18 @@
+function add(price1, price2) {
+      const total = price1 + price2;
+      return total;
+}
+const bill = add(100, 200);
+console.log('Total bill:', bill);
+
+function doMath(num1, num2) {
+      const sum = num1 + num2;
+      const difference = num1 - num2;
+      const multiply = sum * difference;
+      const result = multiply / 2;
+      return result;  };
+const result = doMath(10, 5);
+
+console.log('Result:', result);
+
+
