f0e26efa06dc17380b1bc1d20858308bc9b2ba62
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-05-08 01:26:36 +0600

    return js

---
+++ return.js
@@ -0,0 +1,10 @@
+// return in function   
+function tenTimes(num) {
+      const result = num * 10;
+      return result;
+}
+tenTimes(5);
+const output = tenTimes(5);
+console.log('output:', output);
+const bignumber = tenTimes(90);
+console.log('big number:', bignumber);
