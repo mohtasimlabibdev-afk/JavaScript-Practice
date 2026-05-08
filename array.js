fc63a0ebbe9c808fc058d2529d44bb4fbe54c1ff
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-05-08 20:01:28 +0600

    conver js

---
+++ array.js
@@ -0,0 +1,10 @@
+// Write an Function that give me the sum of all numbers in an array
+// declare a function
+//  
+
+function sumOfNumber(numbers) {
+      console.log('Numbers:', numbers);
+}
+
+const sum = sumOfNumber();
+console.log('Sum of numbers is:', sum);
---
+++ convert.js
@@ -0,0 +1,9 @@
+function inchToFeet(inch) {
+      const feet = inch / 12;
+      return feet;
+}
+function feetToInch(inch){
+
+}
+const shuvoHeight = inchToFeet(75);
+console.log(shuvoHeight);
