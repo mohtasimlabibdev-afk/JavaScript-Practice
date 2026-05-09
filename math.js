b11d64eb1d541a922f7811b161ee1a3071fbfc76
mohtasimlabibdev-afk <mohtasim.labib.dev@gmail.com>
2026-05-10 05:01:02 +0600

    just js math practicing

---
+++ math.js
@@ -0,0 +1,14 @@
+// // math 
+// const Math = {
+//       min: function min (number1, number2) {
+//             return number2;
+// }
+// }
+const min = Math.min(22, 33, 78, -400, 35)
+console.log(min)
+const max = Math.max(22, 33, 78, -400, 35)
+console.log(max)
+console.log(Math.PI)
+console.log(Math.abs(70-100))
+console.log(Math.round(2.99999999))
+console.log(Math.floor(2.99999999))
